2e8a3038b285d714826072d3b8f303f8ad7bffa7
ikhide <ikhide.atakpu@gmail.com>
2021-09-13 21:30:27 +0100

    Todo-list

--- src/App.js
+++ src/App.js
@@ -1,79 +1,91 @@
 import React, { Component } from "react";
-import Register from "./Components/Register";
-import Dashboard from "./Components/Dashboard";
-import Login from "./Components/Login";
-
+import PostLists from "./Components/PostLists";
 export default class App extends Component {
-  state = {
-    page: "register",
-    registrations: [],
-    isLoggedIn: false,
-    loggedInUser: {},
-  };
-
-  gotoLogin = () => {
-    this.setState({ page: "login" });
-  };
-  gotoRegister = () => {
-    this.setState({ page: "register" });
-  };
-
-  loginUser = ({ email, password }) => {
-    const user = this.state.registrations.filter((users) => {
-      return users.email === email && users.password === password;
-    });
-
-    if (user.length > 0) {
-      this.setState({ isLoggedIn: true, loggedInUser: user });
-    } else {
-      alert("incorrect Detail");
-    }
-  };
-
-  registerUser = (data) => {
-    const user = this.state.registrations.filter((users) => {
-      return users.email === data.email;
-    });
-
-    if (user.length > 0) {
-      alert("this email has been used");
-    } else {
-      const registrationList = this.state.registrations;
-      registrationList.push(data);
-      this.setState({ registrations: registrationList });
-      this.gotoLogin();
-    }
-  };
-
-  logOutUser = () => {
-    this.setState((prevState) => {
-      return {
-        ...prevState,
-        page: "login",
-        isLoggedIn: false,
-        loggedInUser: {},
-      };
-    });
-  };
-
   render() {
-    const { isLoggedIn, page } = this.state;
-    console.log("total", this.state);
-    if (isLoggedIn) {
-      return (
-        <Dashboard
-          userDetail={this.state.loggedInUser}
-          logOutUser={this.logOutUser}
-        />
-      );
-    } else if (page === "register") {
-      return (
-        <Register registerUser={this.registerUser} gotoLogin={this.gotoLogin} />
-      );
-    } else {
-      return (
-        <Login loginUser={this.loginUser} gotoRegister={this.gotoRegister} />
-      );
-    }
+    return (
+      <div>
+        <PostLists />
+      </div>
+    );
   }
 }
+
+// import React, { Component } from "react";
+// import Register from "./Components/Register";
+// import Dashboard from "./Components/Dashboard";
+// import Login from "./Components/Login";
+
+// export default class App extends Component {
+//   state = {
+//     page: "register",
+//     registrations: [],
+//     isLoggedIn: false,
+//     loggedInUser: {},
+//   };
+
+//   gotoLogin = () => {
+//     this.setState({ page: "login" });
+//   };
+//   gotoRegister = () => {
+//     this.setState({ page: "register" });
+//   };
+
+//   loginUser = ({ email, password }) => {
+//     const user = this.state.registrations.filter((users) => {
+//       return users.email === email && users.password === password;
+//     });
+
+//     if (user.length > 0) {
+//       this.setState({ isLoggedIn: true, loggedInUser: user });
+//     } else {
+//       alert("incorrect Detail");
+//     }
+//   };
+
+//   registerUser = (data) => {
+//     const user = this.state.registrations.filter((users) => {
+//       return users.email === data.email;
+//     });
+
+//     if (user.length > 0) {
+//       alert("this email has been used");
+//     } else {
+//       const registrationList = this.state.registrations;
+//       registrationList.push(data);
+//       this.setState({ registrations: registrationList });
+//       this.gotoLogin();
+//     }
+//   };
+
+//   logOutUser = () => {
+//     this.setState((prevState) => {
+//       return {
+//         ...prevState,
+//         page: "login",
+//         isLoggedIn: false,
+//         loggedInUser: {},
+//       };
+//     });
+//   };
+
+//   render() {
+//     const { isLoggedIn, page } = this.state;
+//     console.log("total", this.state);
+//     if (isLoggedIn) {
+//       return (
+//         <Dashboard
+//           userDetail={this.state.loggedInUser}
+//           logOutUser={this.logOutUser}
+//         />
+//       );
+//     } else if (page === "register") {
+//       return (
+//         <Register registerUser={this.registerUser} gotoLogin={this.gotoLogin} />
+//       );
+//     } else {
+//       return (
+//         <Login loginUser={this.loginUser} gotoRegister={this.gotoRegister} />
+//       );
+//     }
+//   }
+// }
---
+++ src/Components/PostLists.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class PostLists extends Component {
+  state = {
+    todos: [],
+    newTodo: "",
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((response) => {
+      this.setState({
+        todos: response.data,
+      });
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({ newTodo: e.target.value });
+  };
+
+  addTodo = () => {
+    if (this.state.newTodo.trim() === "") {
+      alert("please enter a todo");
+    } else {
+      const id = this.state.todos.length + 1;
+      const newTodo = {
+        completed: false,
+        id,
+        title: this.state.newTodo,
+        userId: 1,
+      };
+      const todoList = this.state.todos;
+      todoList.push(newTodo);
+      this.setState({ todos: todoList, newTodo: "" });
+    }
+  };
+
+  toggleComplete = (id) => {
+    this.setState({
+      todos: [
+        ...this.state.todos.map((todo) => {
+          if (todo.id === id) todo.completed = !todo.completed;
+
+          return todo;
+        }),
+      ],
+    });
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "50vw",
+            height: "20vh",
+            backgroundColor: "blue",
+            display: "flex",
+            flexDirection: "column",
+            padding: "5%",
+            alignItems: "center",
+          }}
+        >
+          <h1>Todo List</h1>
+          <div>
+            <input
+              value={this.state.newTodo}
+              onChange={(e) => this.handleChange(e)}
+            />
+            <button style={{ backgroundColor: "white" }} onClick={this.addTodo}>
+              Add todo
+            </button>
+          </div>
+        </div>
+        {this.state.todos.reverse().map((todo) => {
+          return (
+            <div
+              style={{ width: "80%", display: "flex", alignItems: "center" }}
+            >
+              <p
+                style={{
+                  textDecoration: todo.completed ? "line-through" : "",
+                  width: "90%",
+                }}
+                key={todo.id}
+              >
+                {todo.title}
+              </p>
+              <button
+                onClick={() => this.toggleComplete(todo.id)}
+                style={{
+                  height: "70%",
+                  width: "80px",
+                  backgroundColor: todo.completed ? "blue" : "white",
+                  color: todo.completed ? "white" : "blue",
+                  border: "none",
+                }}
+              >
+                {todo.completed ? "Undo" : "Done"}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
